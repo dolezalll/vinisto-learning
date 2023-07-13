8387273dc1a45bf9d65eaadc711d9ca4ca3539c1
dolezalll <clamwinantivirus@gmail.com>
2023-07-13 19:21:22 +0200

    add

---
+++ JS/prototypes/script.js
@@ -0,0 +1,39 @@
+const myObject = {
+    city: "Madrid",
+    greet() {
+      console.log(`Greetings from ${this.city}`);
+    },
+  };
+  
+  myObject.greet(); // Greetings from Madrid
+
+  
+  // object lierials 
+
+  // ma jednu property city a metodu greet() 
+
+  // kdyz napisu myObject do konzole vypise to vsechny properties jak predepsane tak mnoho dalsich
+
+
+  // kazdž obekt v js má vbudované funkce a parametry
+  
+
+  myObject.toString(); // "[object Object]"
+
+
+  --> // Object.getPrototypeOf(myObject); // Object { }
+
+
+
+
+
+
+
+
+
+ //  The prototype of an object is not always Object.prototype. Try this:
+
+ 
+
+
+
